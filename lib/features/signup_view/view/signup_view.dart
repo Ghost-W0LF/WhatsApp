9675eacbf3d050904601c8a82ff8c3a99a3e2039
Abhinab Khatri <abhinabkhatri@GhostWolf.local>
2024-09-02 10:21:28 +0545

    fix: add signup Validators

--- lib/features/signup_view/view/signup_view.dart
+++ lib/features/signup_view/view/signup_view.dart
@@ -4,12 +4,14 @@ import 'package:whats_app_ui/features/signup_view/services/signup_service.dart';
 import 'package:whats_app_ui/base/widgets/cust_button.dart';
 import 'package:whats_app_ui/base/widgets/cust_text_formfield.dart';
 import 'package:whats_app_ui/utils/constants/t_text.dart';
+import 'package:whats_app_ui/utils/validator/validator.dart';
 
 class SignupView extends StatelessWidget {
   SignupView({super.key});
   final TextEditingController nameController = TextEditingController();
   final TextEditingController emailComtroller = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final _signUpKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -36,59 +38,75 @@ class SignupView extends StatelessWidget {
               height: 30,
             ),
             Form(
+                key: _signUpKey,
                 child: Column(
-              children: [
-                //
-                //First and last name
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Expanded(
-                      child: CustTextFormField(
-                        controller: nameController,
-                        hintText: Ttext.firstName,
-                        iconData: Icons.person,
-                      ),
+                    //
+                    //First and last name
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          //user name
+                          child: CustTextFormField(
+                            controller: nameController,
+                            validator: userNameValidators,
+                            hintText: Ttext.firstName,
+                            iconData: Icons.person,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    //
+                    //email
+                    CustTextFormField(
+                      controller: emailComtroller,
+                      validator: emailValidators,
+                      hintText: Ttext.email,
+                      iconData: Icons.email,
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    //
+                    //password
+                    CustTextFormField(
+                      controller: passwordController,
+                      validator: passwordValidators,
+                      hintText: Ttext.password,
+                      iconData: Icons.email,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+
+                    const SizedBox(
+                      height: 40,
                     ),
                   ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                //
-                //email
-                CustTextFormField(
-                  controller: emailComtroller,
-                  hintText: Ttext.email,
-                  iconData: Icons.email,
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                //
-                //password
-                CustTextFormField(
-                  controller: passwordController,
-                  hintText: Ttext.password,
-                  iconData: Icons.email,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-
-                const SizedBox(
-                  height: 40,
-                ),
-              ],
-            )),
+                )),
             //
             //Signup button
             CustButton(
               text: Ttext.creatAccount,
               onPressed: () {
+                if (_signUpKey.currentState!.validate()) {
+                  register.registerUser(emailComtroller.text,
+                      nameController.text, passwordController.text);
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Processing Data')),
+                  );
+                } else {
+                  debugPrint("error");
+                }
+              }, /* {
+
                 register.registerUser(emailComtroller.text, nameController.text,
                     passwordController.text);
-              },
+              }, */
             )
           ],
         ),
--- lib/utils/validator/validator.dart
+++ lib/utils/validator/validator.dart
@@ -1,22 +1,30 @@
-//Login email validator
- String? emailValidators(value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your email';
-                  }
-                  if (!value.contains('@') || !value.contains('.com')) {
-                    return 'Please enter a valid email || "@"".com"';
-                  }
-                  return null;
-                }
-//login password validator
-String? passwordValidators(value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your password';
-                  }
-                  if (value.length != 8) {
-                    return 'Short Password';
-                  }
-                  return null;
-                }
+// Email validator
+String? emailValidators(value) {
+  if (value == null || value.isEmpty) {
+    return 'Please enter your email';
+  }
+  if (!value.contains('@') || !value.contains('.com')) {
+    return 'Please enter a valid email || "@"".com"';
+  }
+  return null;
+}
 
-                
+// Eassword validator
+String? passwordValidators(value) {
+  if (value == null || value.isEmpty) {
+    return 'Please enter your password';
+  }
+  if (value.length != 8) {
+    return 'Short Password';
+  }
+  return null;
+}
+
+//Username validator
+String? userNameValidators(value) {
+  if (value == null || value.isEmpty) {
+    return 'Please enter your UserName';
+  }
+
+  return null;
+}
