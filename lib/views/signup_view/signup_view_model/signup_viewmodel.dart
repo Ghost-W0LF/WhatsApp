6864cb1b65160e4343409695e5c0d41e0cf7f092
Abhinab Khatri <abhinabkhatri@GhostWolf.local>
2024-09-08 11:21:00 +0545

    chore: minor changes

--- lib/views/signup_view/signup_view_model/signup_viewmodel.dart
+++ lib/views/signup_view/signup_view_model/signup_viewmodel.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:whats_app_ui/base/components/snack_bar/snackbar_service.dart';
-import 'package:whats_app_ui/views/signup_view/services/signup_service.dart';
+
+import 'package:whats_app_ui/views/signup_view/signup_repository/signup_repository.dart';
 
 class SignupViewmodel extends ChangeNotifier {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController emailComtroller = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final GlobalKey<FormState> signUpKey = GlobalKey<FormState>();
-  final register = SignupService();
+
+  final register = SignupRepository();
 
   void createAccount() {
-    if (signUpKey.currentState!.validate()) {
-      register.registerUser(
-          emailComtroller.text, nameController.text, passwordController.text);
-      SnackBarService.showSnackBar(
-          content: "Processing Data",
-          duration: const Duration(milliseconds: 400));
-    } else {
-      debugPrint("error");
+    try {
+      if (signUpKey.currentState!.validate()) {
+        register.signupAuth(
+            emailComtroller.text, nameController.text, passwordController.text);
+        SnackBarService.showSnackBar(
+            content: "Processing Data",
+            duration: const Duration(milliseconds: 400));
+      }
+    } catch (e) {
+      debugPrint("The error is:- $e");
     }
   }
 
