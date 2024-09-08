8fcf0cbf00b446d7baab6ff0fd60991619dfdf7a
Abhinab Khatri <abhinabkhatri@GhostWolf.local>
2024-09-08 11:13:15 +0545

    fix: add repository to signup

--- lib/views/signup_view/services/signup_service.dart
+++ lib/views/signup_view/services/signup_service.dart
@@ -1,33 +1,19 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
+import 'package:whats_app_ui/base/networking/dio_instance.dart';
 import 'package:whats_app_ui/utils/constants/t_url.dart';
-import 'package:whats_app_ui/views/login_view/view_model/interceptor/auth_interceptor.dart';
 
 class SignupService {
-  final dio = Dio();
-  SignupService() {
-    dio.interceptors.add(AuthInterceptor());
-    dio.interceptors.add(LogInterceptor());
-  }
-  Future<void> registerUser(
+  final dio = DioInstance().dio;
+
+  Future<Response> registerUser ( 
       String email, String fullname, String password) async {
-    try {
-      final response = await dio.post(TUrl.signUpUrl, data: {
+
+    return  await dio.post(TUrl.signUpUrl, data: {
         "full_name": fullname,
         "email": email,
         "password": password,
-      });
+      });   
+    } 
 
-      if (response.statusCode == 200) {
-        final data = response.data;
-        debugPrint('User ID: ${data['full_name']}');
-        debugPrint("Register Success");
-      } else {
-        debugPrint("Register Failed");
-        debugPrint('Login failed: ${response.statusCode} ');
-      }
-    } catch (e) {
-      debugPrint("error:$e");
-    }
   }
-}
+
---
+++ lib/views/signup_view/signup_repository/signup_repository.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:whats_app_ui/base/networking/dio_instance.dart';
+import 'package:whats_app_ui/views/signup_view/services/signup_service.dart';
+
+class SignupRepository {
+  final dio = DioInstance().dio;
+  SignupService signupService = SignupService();
+  Future<void> signupAuth(
+      String email, String fullname, String password) async {
+    final response =
+        await signupService.registerUser(email, fullname, password);
+    try {
+      if (response.statusCode == 200) {
+        final data = response.data;
+        debugPrint('User ID: ${data['full_name']}');
+        debugPrint("Register Success");
+      } else {
+        debugPrint("Register Failed");
+        debugPrint('Login failed: ${response.statusCode} ');
+      }
+    } catch (e) {
+      debugPrint('Login failed: $e');
+    }
+  }
+}
